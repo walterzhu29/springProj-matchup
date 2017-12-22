1e9fd3b1f0d665cd9e727d48cdbdf42ded06b9ed
YuqiZhu29 <yzhu29@stevens.edu>
2017-12-22 15:38:24 -0500

    use google freebusy query

--- src/main/java/org/ez/springProj/springProjmatchup/rest/MatchupREST.java
+++ src/main/java/org/ez/springProj/springProjmatchup/rest/MatchupREST.java
@@ -1,118 +1,35 @@
 package org.ez.springProj.springProjmatchup.rest;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.client.util.Data;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.CalendarScopes;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.*;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.ez.springProj.springProjmatchup.query.GoogleCalendarService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 @RestController
 @RequestMapping(value = "/matchup")
 public class MatchupREST {
     private final static Logger logger = LoggerFactory.getLogger(MatchupREST.class);
-
-//    /** Application name. */
-//    private static final String APPLICATION_NAME =
-//            "springProj-matchup";
-//
-//    /** Directory to store user credentials for this application. */
-//    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-//            System.getProperty("user.home"), "desktop/springProj-matchup");
-//
-//    /** Global instance of the {@link FileDataStoreFactory}. */
-//    private static FileDataStoreFactory DATA_STORE_FACTORY;
-//
-//    /** Global instance of the JSON factory. */
-//    private static final JsonFactory JSON_FACTORY =
-//            JacksonFactory.getDefaultInstance();
-//
-//    /** Global instance of the HTTP transport. */
-//    private static HttpTransport HTTP_TRANSPORT;
-//
-//    /** Global instance of the scopes required by this quickstart.
-//     *
-//     * If modifying these scopes, delete your previously saved credentials
-//     * at ~/.credentials/calendar-java-quickstart
-//     */
-//    private static final List<String> SCOPES =
-//            Arrays.asList(CalendarScopes.CALENDAR_READONLY);
-//
-//    static {
-//        try {
-//            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-//            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-//        } catch (Throwable t) {
-//            t.printStackTrace();
-//            System.exit(1);
-//        }
-//    }
-//
-//    /**
-//     * Creates an authorized Credential object.
-//     * @return an authorized Credential object.
-//     * @throws IOException
-//     */
-//    public static Credential authorize() throws IOException {
-//        // Load client secrets.
-//        InputStream in =
-//                MatchupREST.class.getResourceAsStream("/client_secret.json");
-//        GoogleClientSecrets clientSecrets =
-//                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-//
-//        // Build flow and trigger user authorization request.
-//        GoogleAuthorizationCodeFlow flow =
-//                new GoogleAuthorizationCodeFlow.Builder(
-//                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-//                        .setDataStoreFactory(DATA_STORE_FACTORY)
-//                        .setAccessType("offline")
-//                        .build();
-//        Credential credential = new AuthorizationCodeInstalledApp(
-//                flow, new LocalServerReceiver()).authorize("user");
-//        System.out.println(
-//                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-//        return credential;
-//    }
-//
-//    /**
-//     * Build and return an authorized Calendar client service.
-//     * @return an authorized Calendar client service
-//     * @throws IOException
-//     */
-//    public static com.google.api.services.calendar.Calendar
-//    getCalendarService() throws IOException {
-//        Credential credential = authorize();
-//        return new com.google.api.services.calendar.Calendar.Builder(
-//                HTTP_TRANSPORT, JSON_FACTORY, credential)
-//                .setApplicationName(APPLICATION_NAME)
-//                .build();
-//    }
     /**
      * give a calendarID,
      * @return a list of upcoming events.
@@ -130,7 +47,7 @@ public class MatchupREST {
 
         // List the next 10 events from the primary calendar.
         DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("u9udoeo3tukabp92990sbe3c2s@group.calendar.google.com")
+        Events events = service.events().list(calendarId)
                 .setMaxResults(10)
                 .setTimeMin(now)
                 .setOrderBy("startTime")
@@ -155,6 +72,39 @@ public class MatchupREST {
     /**
      * give calendarID and a time interval,
      * @return a list busy infos
+     * @throws IOException
      */
+    @ApiOperation(value = "busy-infos", notes = "return busy infos for the given time interval")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "calendarId", dataType = "String", required = true),
+            @ApiImplicitParam(paramType = "query", name = "timeMin", dataType = "String", required = true),
+            @ApiImplicitParam(paramType = "query", name = "timeMax", dataType = "String", required = true)
+    })
+    @RequestMapping(value = "/busy-infos", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<FreeBusyResponse> checkBusyInfos(@RequestParam(name = "calendarId") String calendarId,
+                                                 @RequestParam(name = "timeMin") String timeMin,
+                                                 @RequestParam(name = "timeMax") String timeMax) throws IOException, ParseException {
+
+
+        com.google.api.services.calendar.Calendar service =
+                GoogleCalendarService.getCalendarService();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date timeMi = df.parse(timeMin);
+        DateTime startTime = new DateTime(timeMi, TimeZone.getDefault());
+        Date timeMa = df.parse(timeMax);
+        DateTime endTime = new DateTime(timeMa, TimeZone.getDefault());
+        FreeBusyRequest req = new FreeBusyRequest();
+        List<FreeBusyRequestItem> items= new ArrayList<FreeBusyRequestItem>();
+        FreeBusyRequestItem item = new FreeBusyRequestItem();
+        item.setId(calendarId);
+        items.add(item);
+        req.setItems(items);
+        req.setTimeMin(startTime);
+        req.setTimeMax(endTime);
+        Calendar.Freebusy.Query fbq= service.freebusy().query(req);
+        FreeBusyResponse fbResponse = fbq.execute();
+
+        return new ResponseEntity<>(fbResponse, HttpStatus.OK);
+    }
 
 }
